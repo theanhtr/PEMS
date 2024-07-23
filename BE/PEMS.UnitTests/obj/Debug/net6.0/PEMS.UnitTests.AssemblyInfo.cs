e73a447465fd1a612d29cea41de16034ad2191cb
theanhtr <trtheanh.work@gmail.com>
2024-07-23 23:00:20 +0700

    update dockerfile

--- BE/PEMS.UnitTests/obj/Debug/net6.0/PEMS.UnitTests.AssemblyInfo.cs
+++ BE/PEMS.UnitTests/obj/Debug/net6.0/PEMS.UnitTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PEMS.UnitTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7051f5af21b8ed38cb5542d27a334ab7608c828")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d283141eb4aaae5b78d7f3afb02abe89d8e02fdb")]
 [assembly: System.Reflection.AssemblyProductAttribute("PEMS.UnitTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PEMS.UnitTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
