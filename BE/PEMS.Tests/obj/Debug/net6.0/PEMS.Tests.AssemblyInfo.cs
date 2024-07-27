4e8e120a0683d2b62f41bbc6abff6210cc33484a
theanhtr <trtheanh.work@gmail.com>
2024-07-27 13:46:22 +0700

    update job auto rollout

--- BE/PEMS.Tests/obj/Debug/net6.0/PEMS.Tests.AssemblyInfo.cs
+++ BE/PEMS.Tests/obj/Debug/net6.0/PEMS.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PEMS.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e73a447465fd1a612d29cea41de16034ad2191cb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c350c6a094e5ddcb0618f453869ae80efb83bd0e")]
 [assembly: System.Reflection.AssemblyProductAttribute("PEMS.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PEMS.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
