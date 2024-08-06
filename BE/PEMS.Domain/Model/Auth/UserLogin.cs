850122d9d0daa2f9b3b7dada4edac46ef51d53aa
theanhtr <trtheanh.work@gmail.com>
2024-08-06 22:17:29 +0700

    đổi cách lưu mật khẩu

--- BE/PEMS.Domain/Model/Auth/UserLogin.cs
+++ BE/PEMS.Domain/Model/Auth/UserLogin.cs
@@ -6,13 +6,11 @@ namespace PEMS.Domain
     {
         #region Fields
         /// <summary>
-        /// Số thứ tự cột của tiêu đề
         /// </summary>
         [Required]
         public string Username { get; set; }
 
         /// <summary>
-        /// Tên của cột trong server
         /// </summary>
         [Required]
         public string Password { get; set; }
--- BE/PEMS/obj/Debug/net6.0/PEMS.AssemblyInfo.cs
+++ BE/PEMS/obj/Debug/net6.0/PEMS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PEMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e27f9717d00f8e71633eca05793275fab71eb8b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a70231eec22d542a8084353fa23b2e6b13937000")]
 [assembly: System.Reflection.AssemblyProductAttribute("PEMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PEMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
