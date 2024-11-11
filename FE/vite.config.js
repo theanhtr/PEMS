e22ea92c81440cbad4b24648b49d6125b1c47962
theanhtr <trtheanh.work@gmail.com>
2024-11-11 21:41:50 +0700

    update build ui

--- FE/vite.config.js
+++ FE/vite.config.js
@@ -1,13 +1,26 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import { resolve } from "path";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      "@": resolve(__dirname, "src"),
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    server: {
+      port: 6789,
     },
-  },
+    plugins: [
+      vue()
+    ],
+    resolve: {
+      alias: {
+        "@": resolve(__dirname, "src"),
+      },
+    },
+    base:
+      command === "build"
+        ? `${env.VITE_PREFIX_CDN_URL}`
+        : "/"
+  }
 });
